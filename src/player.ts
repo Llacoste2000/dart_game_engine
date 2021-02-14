3f9676e5b5b9a7eae31627ef47160ec6a1f73e8a
Louis <louis.lacoste@ynov.com>
2021-02-14 23:29:18 +0100

    .

--- src/player.ts
+++ src/player.ts
@@ -1,42 +1,43 @@
-import {Board, BoardZone} from "./board.enum";
+import { Board, BoardZone } from "./board.enum";
+import { GameModeBase } from "./gamemode";
 
 const readline = require("readline-sync");
 
 export class Player {
-    name: string;
-    email: string;
-    gameWin: number;
-    gameLost: number;
-    score: any;
+	name: string;
+	email: string;
+	gameWin: number;
+	gameLost: number;
+	score: any;
 
-    constructor(name: string, email: string, gameWin: number, gameLost: number) {
-        this.name = name;
-        this.email = email;
-        this.gameWin = gameWin;
-        this.gameLost = gameLost;
-    }
+	constructor(name: string, email: string, gameWin: number, gameLost: number) {
+		this.name = name;
+		this.email = email;
+		this.gameWin = gameWin;
+		this.gameLost = gameLost;
+	}
 
-    /**
-     * Play the turn of the player
-     */
-    play(): Promise<any> {
-        return new Promise<any>((resolve) => {
-            let board, zone;
+	/**
+	 * Play the turn of the player
+	 */
+	play(): Promise<any> {
+		return new Promise<any>((resolve) => {
+			let board, zone;
 
-            do {
-                board = readline.question(`Quel est le chiffre  ?`);
-                zone = readline.question(`Quelle est la zone ?`);
+			do {
+				board = readline.question(`Quel est le chiffre  ?`);
+				zone = readline.question(`Quelle est la zone ?`);
+			} while (Board[board] === undefined || BoardZone[zone] === undefined);
 
-            } while (Board[board] === undefined || BoardZone[zone] === undefined);
+			console.log(
+				`Ton tir est ${Board[board]} dans la zone ${BoardZone[zone]}`,
+			);
+			// @ts-ignore
+			resolve([Board[Board[board]], BoardZone[BoardZone[zone]]]);
+		});
+	}
 
-            console.log(`Ton tir est ${Board[board]} dans la zone ${BoardZone[zone]}`);
-            // @ts-ignore
-            resolve([Board[Board[board]], BoardZone[BoardZone[zone]]]);
-        });
-    }
-
-    setScore(score: any): void {
-        this.score = score;
-    }
-
+	setScore(score: any): void {
+		this.score = score;
+	}
 }
